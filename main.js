22d579a31c99ad569647c1fbae7a86bd8fb354b9
Fernando Cate <fernandoecate@gmail.com>
2022-08-02 15:28:05 -0300

    Se agregaron lupulos. Ahora pide al usuario el ingreso de datos mediante prompt

--- main.js
+++ main.js
@@ -3,29 +3,79 @@ class Cerveza{
 
     constructor(estilo, densidadInicial, densidadfinal){
         this.estilo = estilo        
-        this.densidadInicial = densidadInicial
-        this.densidadfinal = densidadfinal        
+        this.densidadInicial = parseFloat(densidadInicial)
+        this.densidadfinal = parseFloat(densidadfinal)        
     }
     
     calcularAlcohol(){
-        /*
-        Densidad inicial - densidad final x 105   para obtener peso del alcohol
-        peso del alcohol x 1,25 para obtener el volumen en %  
-        */
+       
        this.alc = (this.densidadInicial - this.densidadfinal) * 105 
        this.abv = ((this.alc * 1.25) / 1000 ).toFixed(2)
-       console.log(`Volumen alcoholico de la ${this.estilo} : ${this.abv}%`)    
-             
+       console.log(`Volumen alcoholico de la ${this.estilo} : ${this.abv}%`)               
     }
     
+    mostrar() { 
+        return `Estilo: ${this.estilo} \n  Densidad inicial: ${this.densidadInicial} \n  Densidad final: ${this.densidadfinal} \n volumen alcoholico: ${this.abv} `
+    }  
+
+}
+
+class Malta{
+    constructor(nombre, cantidad = 0){
+        this.nombre = nombre;
+        this.cantidad = parseFloat(cantidad);
+    }
+
     mostrar(){
-        console.log(`Estilo: ${this.estilo} \n  Densidad inicial: ${this.densidadInicial} \n  Densidad final: ${this.densidadfinal} \n volumen alcoholico: ${this.abv} `)
+        return ` Malta = ${this.nombre} - cantidad usada ${this.cantidad} `
     }
 
 }
 
+class Lupulo {
+    constructor(nombre,cantidad,adicion){
+        this.nombre = nombre
+        this.cantidad = cantidad,
+        this.adicion = parseInt(adicion)
+    }
+}
 
 
-let dorada = new Cerveza('Dorada Pampeana', parseFloat(1060), parseFloat(1011))
+const maltas = []
+const lupulos = []
+
+/*           función que pide al usuario las maltas y los lúpulos utilizados           */
+const ingresar = () => {
+    let malta = ' '
+    let lupulo = ' '
+
+    do{
+        malta = prompt('Malta (Aceptar para salir): ') 
+        if(malta === ''){
+            break
+        }   
+        let cantidad = parseFloat(prompt('Cantidad en KG'))
+        maltas.push(new Malta(malta, cantidad)) 
+    } while (malta != '')
+
+
+    do{
+        lupulo = prompt('lupulo (Aceptar para salir): ') 
+        if(lupulo === ''){
+            break
+        }   
+        let cantidad = parseFloat(prompt('Cantidad en GR'))
+        let adicion = parseInt(prompt('Ingrese minuto de adición: '))
+        lupulos.push(new Lupulo(lupulo, cantidad, adicion))
+    } while (lupulo != '')
+
+}
+
+ingresar()
+console.table(maltas)
+console.table(lupulos)
+ 
+
+const dorada = new Cerveza('Dorada Pampeana',1060, 1011)
 dorada.calcularAlcohol()
-dorada.mostrar()
+console.log(dorada.mostrar())
